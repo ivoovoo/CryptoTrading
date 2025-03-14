46882abc47a31b52d57721fab882ad4d98b4e9b5
Ratmir <ceratmir@gmail.com>
2025-03-14 19:24:54 +0600

    blockchain and main7

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Указывает относительный путь для работы на сервере
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
